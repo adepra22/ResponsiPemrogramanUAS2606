cbe4be08c4e0e7dc2021621317e756194c26b46f
adepra22 <ade.prasetyo@students.amikom.ac.id>
2020-07-06 16:36:27 +0700

    Upload Responsi Pemrograman UAS 19.11.2606

---
+++ Produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        // PERINTAH: lengkapi property class produk, sesuai petunjuk soal
+        public string kd_produk { get; set; }
+        public string nama_produk { get; set; }
+        public double harga_beli { get; set; }
+        public double harga_jual { get; set; }
+    }
+}
